7017497b60bb7a644fc52c255c8d23523b669f46
Rodrigo Cruz <00115417@uca.edu.sv>
2018-06-10 14:03:47 -0600

    Se añadió la modalidad 2 jugadores

--- src/abstractfactory/OmegaLand.java
+++ src/abstractfactory/OmegaLand.java
@@ -19,8 +19,12 @@ public class OmegaLand {
 
         Scanner input = new Scanner(System.in);
         boolean flag = true;
+        boolean bandera = true;
+        int waitANDsee = 0;
         AbstractFactory factory;
 
+        
+//jugador 1        
         while (flag) {
             System.out.println("\n¡Bienvenido a Omega Land!\n\nAquí desatarás el poder del bando que tú elijas para alzarte victorioso.\nCada bando tiene aptitudes distintas e ideales para tu estilo de combate,para saber un poco más acerca de ellas escribe <info>.\nPara comenzar a jugar pulsa <A>, para salir <S>:");
             System.out.println("");
@@ -39,10 +43,11 @@ public class OmegaLand {
             if (toLowerCase.equals("a")) {
                 System.out.println("Cargando...");
                 System.out.println("¡Saludos guerrero!\nAntes de comenzar a jugar necesito saber tu nombre: ");
-                String nombreJ1 = input.next();
+                String nombreJ1 = input.nextLine();
                 System.err.println("-------------------------------------------------");
                 System.out.println("Jugador 1: " + nombreJ1);
-                flag=false;
+                waitANDsee = 1;
+                flag = false;
             }
             if (flag == false) {
                 break;
@@ -53,8 +58,47 @@ public class OmegaLand {
                 System.out.println(" ");
             }
         }
-                factory = FactoryProducer.getFactory(1);
-                Edificio central = factory.getEdificio(4);
-                central.build();
+        factory = FactoryProducer.getFactory(1);
+        Edificio centralj1 = factory.getEdificio(4);
+        centralj1.build();
+
+        
+//jugador 2        
+        if (waitANDsee == 1) {
+            while (bandera) {
+                System.out.println("\n¡Bienvenido a Omega Land!\n\nAquí desatarás el poder del bando que tú elijas para alzarte victorioso.\nCada bando tiene aptitudes distintas e ideales para tu estilo de combate,para saber un poco más acerca de ellas escribe <info>.\nPara comenzar a jugar pulsa <A>, para salir <S>:");
+                System.out.println("");
+                String select2 = input.nextLine();
+                System.out.println("");
+                String toLowerCase = select2.toLowerCase();
+                if (toLowerCase.equals("info")) {
+                    new GUI().setVisible(true);
+                    System.out.println("-------------------------------------------------");
+                    System.out.println(" ");
+                }
+                if (toLowerCase.equals("s")) {
+                    System.out.println("¡Gracias por jugar! Vuelve pronto.");
+                    bandera = false;
+                }
+                if (toLowerCase.equals("a")) {
+                    System.out.println("Cargando...");
+                    System.out.println("¡Saludos guerrero!\nAntes de comenzar a jugar necesito saber tu nombre: ");
+                    String nombreJ2 = input.nextLine();
+                    System.err.println("-------------------------------------------------");
+                    System.out.println("Jugador 2: " + nombreJ2);
+                    bandera = false;
+                }
+                if (bandera == false) {
+                    break;
+                }
+                if (!toLowerCase.equals("a") || !toLowerCase.equals("s") || !toLowerCase.equals("info")) {
+                    System.err.println("Ingrese un dato válido");
+                    System.out.println("-------------------------------------------------");
+                }
+            }
+            factory = FactoryProducer.getFactory(1);
+            Edificio centralj2 = factory.getEdificio(4);
+            centralj2.build();
+        }
     }
 }
--- src/abstractfactory/edificios/BaseOperaciones.java
+++ src/abstractfactory/edificios/BaseOperaciones.java
@@ -11,6 +11,7 @@ import abstractfactory.militantes.Militante;
 import abstractfactory.terrestres.Terrestre;
 import abstractfactory.vehiculos.Vehiculo;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
@@ -23,14 +24,16 @@ public class BaseOperaciones implements Edificio {
     ArrayList<Militante> militantes = new ArrayList<>();
     ArrayList<Terrestre> terrestres = new ArrayList<>();
     ArrayList<Vehiculo> vehiculos = new ArrayList<>();
+    Scanner input = new Scanner(System.in);
 
     @Override
     public void build() {
-        System.out.println("Su base de operaciones ha sido creada.\n¡Defiéndala como si su vida dependiera de eso!");
-        int vida = 5500;
-        System.out.println("Vida actual: " + vida);
+        System.out.println("Elija un bando por favor: ");        
+        String bando = input.nextLine();
+        System.out.println("Su base de operaciones ha sido creada.");
+        int vida = 5500, edificios = 0, terrestres = 0, vehiculos = 0;
+        System.out.println("Vida actual: " + vida + "\nEdificios creados actualmente: " + edificios + "\nUnidades terrestres creadas: " + terrestres + "\nVehiculos creados: " + vehiculos);
 
-        System.out.println("Ahora deberías elegir una opción: ");
     }
 
 }
